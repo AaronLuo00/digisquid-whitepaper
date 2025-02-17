97917bf82709c6e7eb8f274065d94c71233d2b99
XiaoLong Luo <xiaolongluo@XiaoLongs-MacBook-Air.local>
2025-02-16 23:44:56 -0500

    change community to random number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ interface Hero {
 export default function Home() {
   const { content, language } = useLanguage()
   const t = {
-    cta: language === 'zh' ? '社区' : 'Community',
+    cta: language === 'zh' ? '社区' : '1231344353',
     learnMore: language === 'zh' ? '了解更多' : 'Learn More'
   }
 
